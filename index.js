65f127531f5d78908c1625336935075090518a0f
Cory Wang <cory.wang11@gmail.com>
2020-07-24 17:44:05 -0700

    fixed looping and display issues

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
+// let messages = [];
+
 function getDogs(number) {
     fetch('https://dog.ceo/api/breeds/image/random/'+ number)
     .then(response => response.json())
     .then(responseJson => 
         displayDogs(responseJson))
-    .catch(error => alert('Warning! Dog API call failed. Try again later.'));
+        .catch(error => alert('Warning! Dog API call failed. Try again later.'));
 }
 
-function templateDogHtml(number){
-    return `<img src="message[${number}]" name="dog-image-${number}" alt="dog pic ${number} class="dog-image">`
+function templateDogHtml(message){
+    console.log(message);
+    return `<img src="${message}" class="dog-image" />`
 }
 
-function displayDogs() {
+function displayDogs(responseJson) {
     /* Log the JSON object from the response */
     console.log(responseJson);
     /* Add each image to the DOM */
-    let message = responseJson.message;
-    for (let i = 0; i < message.length; i++) {
-        $('.dog-pics').append(templateDogHtml(i));
+    messages = responseJson.message;
+    console.log(messages);
+    loopDogs(messages);
+}
+
+function loopDogs(messages) {
+    for (let i = 0; i < messages.length; i++) {
+        $('.dog-pics').append(templateDogHtml(messages[i]));
     };
-    $('dog-pics').removeClass('hidden');
+    $('.dog-pics').removeClass('hidden');
 }
 
 function formEvent() {
     $('form').on('submit', function(event){
         event.preventDefault();
-        let number = $('input');
+        let number = $('input').val();
         console.log(number);
         getDogs(number);
     });
